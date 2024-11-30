9444f82aef1f0175b6f885a53beef7bba765eead
Jagadeesh v <vemulajagadeesh28@gmail.com>
2024-11-30 19:46:37 +0530

    exploring get and post services

--- src/app.js
+++ src/app.js
@@ -3,8 +3,17 @@ const express = require("express");
 /* creating new expressJs application from express method */
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("hello from the server");
+/* Here Order Is Very Important */
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Jagadeesh", lastName: "vemul" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("saved the user data successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("deleted the data successfully");
 });
 
 app.use("/test", (req, res) => {
@@ -13,6 +22,10 @@ app.use("/test", (req, res) => {
 
 app.use("/hello", () => {
   res.send("Hello Hello Hello");
+});
+
+app.use("/", (req, res) => {
+  res.send("hello from the server");
 });
 
 app.listen(7777, () => {
