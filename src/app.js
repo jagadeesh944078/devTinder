15405d51e4415ef7293bd299e619030b3ce40483
Jagadeesh v <vemulajagadeesh28@gmail.com>
2024-12-11 12:42:58 +0530

    cookie,Jwt token

--- src/app.js
+++ src/app.js
@@ -4,8 +4,13 @@ const User = require("./models/user");
 const app = express();
 const { validationSignup } = require("./utils/validation");
 const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
+/* whenever iam reading request i want that data to be parsed into the json then i want to get it */
 app.use(express.json());
+/* whenever you need cookie you need to parse and use it */
+app.use(cookieParser());
 
 app.post("/signup", async (req, res) => {
   try {
@@ -34,10 +39,34 @@ app.post("/login", async (req, res) => {
     }
     const isPasswordValid = await bcrypt.compare(passWord, user.passWord);
     if (isPasswordValid) {
+      /* Create the Jwt token */
+      const token = jwt.sign({ _id: user._id }, "Jagadeesh@#$%2705");
+
+      /* add the token and send the response back to the server */
+      res.cookie("token", token);
+
       res.send("Login Successfull!!");
     } else {
       throw new Error("Invalid Crediantials");
     }
+  } catch (err) {
+    res.status(400).send("Error :" + err.message);
+  }
+});
+
+app.get("/profile", async (req, res) => {
+  try {
+    const cookie = req.cookies;
+    const { token } = cookie;
+    if (!token) {
+      throw new Error("invalid token");
+    }
+    const decoded = jwt.verify(token, "Jagadeesh@#$%2705");
+    const user = await User.findById(decoded._id);
+    if (!user) {
+      throw new Error("user doesnot exist");
+    }
+    res.send(user);
   } catch (err) {
     res.status(400).send("Error :" + err.message);
   }
